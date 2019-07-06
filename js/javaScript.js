b19cbcbc081ef815c3d7668e1ff934a606042d55
Limirida <49957912+limirida@users.noreply.github.com>
2019-07-06 19:55:42 +0300

    Update javaScript.js
    changes are made to a form

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,6 +1,38 @@
 var button = document.querySelector(".search-hotel__legend");
-	var popup = document.querySelector(".form-search");
-		button.addEventListener("click", function (evt) {
+var popup = document.querySelector(".form-search");
+
+var form = popup.querySelector("form");
+
+var arrival = popup.querySelector("[name=date-arrival]");
+var departure = popup.querySelector("[name=date-departure]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+	storage = localStorage.getItem("arrival");
+	storage = localStorage.getItem("departure");
+} catch (err) {
+	isStorageSupport = false;
+}
+
+button.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.toggle("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+	if (!arrival.value || !departure.value) {
 		evt.preventDefault();
-		popup.classList.toggle("modal-show");
+		console.log("Нужно ввести дату заезда и даты выезда");
+	} else {
+		if (isStorageSupport) {
+			localStorage.setItem("arrival", arrival.value);
+			localStorage.setItem("departure", departure.value);
+		} else {
+			function placeOrder() {
+				document.theForm.submit();
+			}
+		}
+	}
 });
